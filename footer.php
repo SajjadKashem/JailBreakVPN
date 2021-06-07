30062f8c0d7e8ba63671bff9292e09224ba3f134
unknown <sajjadkashem24@gmail.com>
2021-06-07 20:31:25 +0600

    Assignment

---
+++ footer.php
@@ -0,0 +1,96 @@
+ <footer class="footer">
+        <div class="container">
+            <div class="footer__btn">
+                <a href="https://jailbreakvpn.com/android/app-universal-release.apk" class="btn">
+                    FREE DOWNLOAD
+                </a>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 col-xs-6">
+                    <a href="http://www.jailbreakvpn.com/" class="logo footer-logo">
+                        <img height="68" width="68" src="<?php echo esc_url( get_template_directory_uri() ); ?>/./World’s Fastest VPN _ Jailbreak VPN_files/logo.png" alt="">
+                        <span class="logo__span">Jailbreak</span> <sup>VPN</sup>
+                    </a>
+                    <p class="footer__copyright">
+                        © 2018 All Rights Reserved
+                    </p>
+                    <ul class="footer__soc">
+                        <li>
+                          <a title="Official Youtube Channel of Jailbreak VPN" href="https://www.youtube.com/channel/UCgNg7ohSLC4XRlGNlhf8gwA">  <span class="icon-youtube"></span></a>
+                        </li>
+                        <li>
+                            <span class="icon-tw"></span>
+                        </li>
+                        <li>
+                            <a title="Official Facebook Page of Jailbreak VPN" href="https://www.facebook.com/jailbreakvpn/"><span class="icon-fb"></span></a>
+                        </li>
+                        <li>
+                            <span class="icon-google"></span>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="col-sm-3 col-xs-6">
+                    <h3 class="footernav__title">Features</h3>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/features/anonymous-ip.html">Anonymous IP</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Free vs. Paid</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Offshore VPN</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/network.html">Our Network</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Secure Internet</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Secure WiFi</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Stream Sports</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">What is a VPN?</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Why JailbreakVPN?</a>
+
+                </div>
+                <div class="col-sm-3 col-xs-6">
+                    <h3 class="footernav__title">Our Apps</h3>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/android.html">Android</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/ios.html">iOS</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/ipad.html">macOS</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/iphone.html">Windows</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/macOS.html">Windows Phone</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Microsoft Surface</a>
+                    <h3 class="footernav__title">Customers</h3>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/checkip.php">Check IP</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Members Area</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">VPN Setup Guide</a>
+                </div>
+                <div class="col-sm-3 col-xs-6">
+                    <h3 class="footernav__title">About</h3>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Support</a>
+                    <a class="footernav__link" href="https://blog.jailbreakvpn.com/">Blog</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Community</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Press</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Terms of Service</a>
+                    <h3 class="footernav__title">Partners</h3>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Affiliates</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Business</a>
+                    <a class="footernav__link" href="http://www.jailbreakvpn.com/">Reseller</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <a style="    position: fixed;
+    z-index: 999;
+    bottom: 0;
+    left: 0;
+    background: #44ccf9;
+    padding: 10px;" href="https://community.jailbreakvpn.com/forums/support-and-questions.4/create-thread" class="signup-btn">
+        <i class="fas fa-life-ring"></i>
+        <span>Support Forum</span>
+    </a>
+
+
+</main>
+
+<div class="arrow-top" style="display: none;">
+    <i class="fas fa-arrow-circle-up"></i>
+</div>
+
+<script defer="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/./World’s Fastest VPN _ Jailbreak VPN_files/scripts.min.js.download"></script>
+
+
+
+<div class="jvectormap-tip" style="left: 353px; top: 3849px; display: none;">United States</div></body></html>
---
+++ header.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<!-- saved from url=(0028)http://www.jailbreakvpn.com/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    
+
+    <title>World’s Fastest VPN | Jailbreak VPN </title>
+    <meta name="description" content="Not All VPN Providers are fast. But Jailbreak VPN offers World&#39;s Fastest VPN service, wifi- security, and 256- bit encryption for 100% private experience, regardless of your country. Try for FREE!">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://jailbreakvpn.com/">
+    <meta property="og:title" content="World’s Fastest VPN for Facebook">
+    <meta property="og:description" content="Not All VPN Providers are fast. But Jailbreak VPN offers World&#39;s Fastest VPN service, wifi- security, and 256- bit encryption for 100% private experience, regardless of your country. Try for FREE!">
+    <meta property="og:image" content="https://www.jailbreakvpn.com/img/screen.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://jailbreakvpn.com/">
+    <meta property="twitter:title" content="World’s Fastest VPN for Twitter">
+    <meta property="twitter:description" content="Not All VPN Providers are fast. But Jailbreak VPN offers World&#39;s Fastest VPN service, wifi- security, and 256- bit encryption for 100% private experience, regardless of your country. Try for FREE!">
+    <meta property="twitter:image" content="https://www.jailbreakvpn.com/img/screen.png">
+
+
+    <link rel="shortcut icon" href="http://www.jailbreakvpn.com/img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="http://www.jailbreakvpn.com/img/favicon/apple-touch-icon-180x180.png">
+
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/./jailBreakVPN/World’s Fastest VPN _ Jailbreak VPN_files/css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/./jailBreakVPN/World’s Fastest VPN _ Jailbreak VPN_files/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/./jailBreakVPN/World’s Fastest VPN _ Jailbreak VPN_files/main.min.css">
+
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#000">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#000">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#000">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script type="text/javascript" async="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/./World’s Fastest VPN _ Jailbreak VPN_files/analytics.js.download"></script><script async="" src="./World’s Fastest VPN _ Jailbreak VPN_files/js"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-70264623-1');
+    </script>
+
+</head>
+
+<body>
+
+<header class="header">
+    <div class="headertop" style="">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="headertop__language">
+                        <a class="headertop__select" data-action="click">
+                            English
+                        </a>
+                        <div class="headertop__list">
+                            <a href="http://www.jailbreakvpn.com/" class="headertop__link">Deutsch</a>
+                            <a href="http://www.jailbreakvpn.com/" class="headertop__link">English</a>
+                            <a href="http://www.jailbreakvpn.com/" class="headertop__link">русский</a>
+                        </div>
+                    </div>
+                    <nav class="headertop__usernav">
+                        <a href="http://www.jailbreakvpn.com/" class="headertop__link">
+                            Login
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="headernav">
+        <div class="container">
+            <div class="container-f">
+                <a href="http://www.jailbreakvpn.com/" class="logo">
+                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/World’s Fastest VPN _ Jailbreak VPN_files/logo.png" alt="">
+                    <span class="logo__span">Jailbreak</span> <sup>VPN</sup>
+                </a>
+                <div class="headernav__menu">
+                    <div class="headermainmenu">
+                        <ul class="nav">
+                            <li><a href="http://www.jailbreakvpn.com/index.html">Overview </a></li>
+                            <li><a href="http://www.jailbreakvpn.com/why-vpn.html">Why VPN? </a></li>
+                            <li><a href="http://www.jailbreakvpn.com/network.html">Our Network </a></li>
+                            <li><a href="http://www.jailbreakvpn.com/allapps.html">Download VPN </a></li>
+                            <li><a href="http://www.jailbreakvpn.com/proxy.html">Free Proxy </a></li>
+                            <li><a href="http://www.jailbreakvpn.com/pricing.html">Pricing </a></li>
+                        </ul>
+                    </div>
+                    <a href="https://jailbreakvpn.com/android/app-universal-release.apk" class="signup-btn">
+                        <i class="fas fa-sign-in-alt"></i>
+                        <span>FREE DOWNLOAD</span>
+                    </a>
+                    <div class="burger">
+                        <span class="burger_line"></span>
+                        <span class="txt-menu">Menu</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</header>
